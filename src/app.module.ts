9d78bd92d0bf5ffd64aea4543a5304264b45bdaa
Bobby Mundy <rmundy@protonmail.com>
2022-02-01 20:16:50 -0500

    added axios & http module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { SchedulesController } from './schedules/schedules.controller';
 import { SchedulesModule } from './schedules/schedules.module';
 import { SchedulesService } from './schedules/schedules.service';
 
 @Module({
-  imports: [SchedulesModule],
+  imports: [SchedulesModule, HttpModule],
   controllers: [SchedulesController],
   providers: [SchedulesService],
 })
--- src/schedules/schedules.module.ts
+++ src/schedules/schedules.module.ts
@@ -1,9 +1,12 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
-import { SchedulesService } from './schedules.service';
+
 import { SchedulesController } from './schedules.controller';
+import { SchedulesService } from './schedules.service';
 
 @Module({
+  imports: [HttpModule],
   controllers: [SchedulesController],
   providers: [SchedulesService]
 })
-export class SchedulesModule {}
+export class SchedulesModule { }
